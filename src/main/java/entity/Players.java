202ecfcedac9c74563013d40d646afa2e04dd5f2
Mikhail Bakulin <mibak@yandex.ru>
2024-02-05 13:45:02 +0300

    Create project Tennis Scoreboard. Create 2 tables at the H2 data base. Create Entities of the tables.

---
+++ src/main/java/entity/Players.java
@@ -0,0 +1,16 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Players {
+    private Integer id;
+    private String name;
+}
