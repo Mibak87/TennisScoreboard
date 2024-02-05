421ed5e44c9ec00a9a5fc7104aa4dd5a43fb7d20
Mikhail Bakulin <mibak@yandex.ru>
2024-02-05 21:36:32 +0300

    Add controllers classes.

---
+++ src/main/java/controller/MatchScoreController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "MatchScoreController", value = "/match-score")
+public class MatchScoreController extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+}
---
+++ src/main/java/controller/NewMatchController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "NewMatchController", value = "/new-match")
+public class NewMatchController extends HttpServlet {
+
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+}
