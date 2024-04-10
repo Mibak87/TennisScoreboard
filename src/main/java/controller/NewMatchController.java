f11b938dbfb97f8c491debb65335da1aee335524
Mikhail Bakulin <mibak@yandex.ru>
2024-04-10 15:56:10 +0400

    Refactored NewMatchController class.

--- src/main/java/controller/NewMatchController.java
+++ src/main/java/controller/NewMatchController.java
@@ -8,8 +8,6 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import org.hibernate.HibernateException;
-import repository.PlayersRepository;
 import service.Match;
 import service.MatchMap;
 
@@ -26,16 +24,12 @@ public class NewMatchController extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String player1Name = request.getParameter("player1");
         String player2Name = request.getParameter("player2");
-            try {
-                Players player1 = new Players(player1Name);
-                Players player2 = new Players(player2Name);
-                UUID matchId = UUID.randomUUID();
-                Match currentMatch = new Match(player1, player2);
-                MatchMap.updateCurrentMatch(matchId, currentMatch);
-                response.sendRedirect("match-score?uuid=" + matchId);
-            } catch (HibernateException e) {
-                e.printStackTrace();
-            }
+        Players player1 = new Players(player1Name);
+        Players player2 = new Players(player2Name);
+        UUID matchId = UUID.randomUUID();
+        Match currentMatch = new Match(player1, player2);
+        MatchMap.updateCurrentMatch(matchId, currentMatch);
+        response.sendRedirect("match-score?uuid=" + matchId);
     }
 
 }
