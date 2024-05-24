00b4a63d18dbaa53988da58447fedd789ad09ff5
Mikhail Bakulin <mibak@yandex.ru>
2024-05-24 15:14:36 +0400

    Refactored HibernateUtil class.

--- src/main/java/utils/HibernateUtil.java
+++ src/main/java/utils/HibernateUtil.java
@@ -11,43 +11,45 @@ public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
     static {
+        PlayersRepository playersRepository = new PlayersRepository();
+        MatchesRepository matchesRepository = new MatchesRepository();
         Players player1 = new Players("Джокович");
-        new PlayersRepository().save(player1);
+        playersRepository.save(player1);
         Players player2 = new Players("Алькараз");
-        new PlayersRepository().save(player2);
+        playersRepository.save(player2);
         Matches match1 = new Matches(player1,player2,player1);
-        new MatchesRepository().save(match1);
+        matchesRepository.save(match1);
 
         Players player3 = new Players("Синнер");
-        new PlayersRepository().save(player3);
+        playersRepository.save(player3);
         Players player4 = new Players("Медведев");
-        new PlayersRepository().save(player4);
+        playersRepository.save(player4);
         Matches match2 = new Matches(player3,player4,player4);
-        new MatchesRepository().save(match2);
+        matchesRepository.save(match2);
 
         Players player5 = new Players("Рублёв");
-        new PlayersRepository().save(player5);
+        playersRepository.save(player5);
         Players player6 = new Players("Зверев");
-        new PlayersRepository().save(player6);
+        playersRepository.save(player6);
         Matches match3 = new Matches(player5,player6,player6);
-        new MatchesRepository().save(match3);
+        matchesRepository.save(match3);
 
         Players player7 = new Players("Руне");
-        new PlayersRepository().save(player7);
+        playersRepository.save(player7);
         Players player8 = new Players("Гуркач");
-        new PlayersRepository().save(player8);
+        playersRepository.save(player8);
         Matches match4 = new Matches(player7,player8,player7);
-        new MatchesRepository().save(match4);
+        matchesRepository.save(match4);
 
         Players player9 = new Players("Рууд");
-        new PlayersRepository().save(player9);
+        playersRepository.save(player9);
         Matches match5 = new Matches(player9,player6,player6);
-        new MatchesRepository().save(match5);
+        matchesRepository.save(match5);
 
         Players player10 = new Players("Умбер");
-        new PlayersRepository().save(player10);
+        playersRepository.save(player10);
         Matches match6 = new Matches(player5,player10,player5);
-        new MatchesRepository().save(match6);
+        matchesRepository.save(match6);
     }
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
--- src/test/java/service/MatchScoreTest.java
+++ src/test/java/service/MatchScoreTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -48,20 +49,22 @@ class MatchScoreTest {
         testMatch.setPlayer2Points("40");
         testMatch.setDeuce(true);
         MatchMap.updateCurrentMatch(matchId, testMatch);
+        new MatchScore().getMatchScore(matchId,1);
         int player1Game = MatchMap.currentMatch.get(matchId).getPlayer1Set().get(0);
         assertNotEquals(1,player1Game);
     }
 
     @Test
     void tieBreakActivate() {
-        List<Integer> player1Set = new ArrayList<>();
-        List<Integer> player2Set = new ArrayList<>();
-        player1Set.add(5);
-        player2Set.add(6);
+        List<Integer> player1Set = testMatch.getPlayer1Set();
+        List<Integer> player2Set = testMatch.getPlayer2Set();
+        player1Set.set(0,5);
+        player2Set.set(0,6);
         testMatch.setPlayer1Set(player1Set);
         testMatch.setPlayer2Set(player2Set);
         testMatch.setPlayer1Points("40");
         MatchMap.updateCurrentMatch(matchId, testMatch);
+        new MatchScore().getMatchScore(matchId,1);
         boolean tieBreak = MatchMap.currentMatch.get(matchId).isTieBreak();
         assertTrue(tieBreak);
     }
